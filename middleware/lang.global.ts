f8bda5377912f0de72d1da19f01e9471c04aab8c
warflash <nwiesinger67@gmail.com>
2023-02-11 01:23:30 +0100

    1 file modified

--- middleware/lang.global.ts
+++ middleware/lang.global.ts
@@ -1,10 +1,9 @@
 export default defineNuxtRouteMiddleware(async (to) => {
-  const path = to.fullPath;
-  console.log('awdoh');
-  let target = path;
-  if (!path.startsWith('/en')) {
-    target = `/en/${target}`;
-  }
-
-  return navigateTo(target);
+  // const path = to.fullPath;
+  // console.log('awdoh');
+  // let target = path;
+  // if (!path.startsWith('/en')) {
+  //   target = `/en/${target}`;
+  // }
+  // return navigateTo(target);
 });
